c49a2c7ef36c88ab90e32c2b9865edd4b86ddd76
Can Lo <clo51@my.centennialcollege.ca>
2018-10-18 19:05:48 -0400

    Initial Commit

---
+++ MidtermTest-300941213Tests/MidtermTest_300941213Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MidtermTest_300941213Tests.swift
+//  MidtermTest-300941213Tests
+//
+//  Created by Đậu Thư on 2018-10-18.
+//  Copyright © 2018 Can Lo. All rights reserved.
+//
+
+import XCTest
+@testable import MidtermTest_300941213
+
+class MidtermTest_300941213Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
